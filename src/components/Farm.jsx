b53d4b836e6bfdc2ce81ae4032d48b7d4d93f523
Teona <teonazaalishvili2@gmail.com>
2023-04-01 22:44:23 -0400

    adding components

---
+++ src/components/Farm.jsx
@@ -0,0 +1,27 @@
+import React, {useState,useEffect} from 'react'
+import Trees from './Trees'
+
+export default function Farm() {
+    let [apples, setApples] = useState(0);
+    let [oranges, setOranges] = useState(0);
+    let [cost, setCost] = useState(0)
+
+    useEffect(() => {
+        const APPLES_PRICE = 2
+        const ORANGES_PRICE = 3
+        let applesSubtotal = apples * APPLES_PRICE
+        let orangesSubtotal = oranges * ORANGES_PRICE
+        let total = applesSubtotal + orangesSubtotal
+        setCost(total)
+
+    }, [ apples, oranges ]);
+
+  return (
+    <div>
+         <div>Cost: {cost}</div>
+         <Trees fruit = {apples} setFruit={setApples}/>
+         <Trees fruit = {oranges} setFruit={setOranges}/>
+      
+    </div>
+  )
+}
